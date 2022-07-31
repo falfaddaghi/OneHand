ba5a5cb6672fee2e3df998ce7c8d44f26e9be5af
falfaddaghi <faisalalfaddaghi@gmail.com>
2022-07-31 23:20:26 +0300

    Add a simple json response

--- Program.cs
+++ Program.cs
@@ -3,4 +3,8 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/query", async (ctx) =>
+{
+    await ctx.Response.WriteAsJsonAsync(new { Name = "Faisal", Age = 20 });
+});
 app.Run("http://localhost:5500");
